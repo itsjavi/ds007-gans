5721fdfea0a0147d32df35eadd953a320118e6a0
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2022-07-14 09:58:43 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ import apis
 
 def lambda_handler(event, context):
     apis.import_weather_data()
-    weather_df = apis.get_weathers_df().sample()
+    weather_df = apis.get_weathers_df().tail(5)
    
     return {
         'weather': json.loads(weather_df.to_json()),
