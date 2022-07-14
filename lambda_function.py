ccc6ab39b46b4879d491ada0758d1c50d187a2f8
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2022-07-14 10:13:54 +0200

    better json structure

--- lambda_function.py
+++ lambda_function.py
@@ -2,11 +2,15 @@ import json
 import requests
 import apis
 
+def df_to_jsonable_array(df):
+    return json.loads(json.dumps(df.to_dict('records'), default=str))
+
+
 def lambda_handler(event, context):
-    apis.import_weather_data()
+    # apis.import_weather_data()
     weather_df = apis.get_weathers_df().tail(5)
    
     return {
-        'weather': json.loads(weather_df.to_json()),
+        'weather': df_to_jsonable_array(weather_df),
         'flights': '' # TODO
     }
